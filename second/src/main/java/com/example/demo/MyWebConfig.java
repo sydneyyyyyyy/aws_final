f656ba4c7ec95ba773e08351f3df762386554472
syd <qwer1478792@gmail.com>
2023-02-03 15:08:20 +0900

    path 수정

--- second/src/main/java/com/example/demo/MyWebConfig.java
+++ second/src/main/java/com/example/demo/MyWebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyWebConfig implements WebMvcConfigurer {
 	
 	//linux
-	static final public String savePath = "usr/upload";
+	static final public String savePath = "/usr/upload";
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
